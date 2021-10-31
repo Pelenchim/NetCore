3b6a99824bdf1395ca16c3ab18678c42693fc9c7
Francisco Fernandez Lopez <pelenchim1@gmail.com>
2021-10-30 23:42:25 -0600

    Insert Category API Method

---
+++ FrontEnd/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business;
+using Newtonsoft.Json;
+
+namespace FrontEnd.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        public object SaveCategory(object ObjIns)
+        {
+            CatCategory Ins = JsonConvert.DeserializeObject<CatCategory>(ObjIns.ToString());
+            Ins.Save(Ins);
+            return true;
+        }
+    }
+}
