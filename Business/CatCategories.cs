d17bdbb4a5259c05e468aafb949a4e941e844fbc
Francisco Fernandez Lopez <pelenchim1@gmail.com>
2021-10-30 23:41:04 -0600

    Insert Category API Method

---
+++ Business/CatCategories.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Data;
+
+namespace Business
+{
+    public class CatCategory
+    {
+        private string TableName = "TblCategory";
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+        public int State { get; set; }
+
+        public Object Save(CatCategory Inst)
+        {
+            try
+            {
+                SqlADOConnection.InitConnection("sa", "1234");
+                return SqlADOConnection.SQLM.InsertObject(TableName, Inst);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }    
+}
